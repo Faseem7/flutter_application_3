412012830c41340392b43f6736978fc83557494f
Faseem7 <76933640+Faseem7@users.noreply.github.com>
2023-09-24 19:43:51 +0530

    added category items

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,11 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_3/models/category.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<CategoryModel> categories = [];
+
+  void _getCategories() {
+    categories = CategoryModel.getCategories();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    _getCategories();
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: appBar(),
@@ -13,69 +26,126 @@ class HomePage extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           _searchField(),
-          SizedBox(height: 20,),
-          Column(
-            children: [
-              Text('Category',
-              style: TextStyle(
-                color: Colors.black,
-                             ),)
-            ],
-          )
+          const SizedBox(
+            height: 37,
+          ),
+          const Padding(
+            padding: EdgeInsets.only(left: 8.0),
+            child: Column(
+              children: [
+                Text(
+                  'Category',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w600,
+                      fontSize: 20),
+                )
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          category_items()
         ],
       ),
     );
   }
 
-  Container _searchField() {
-    return Container(
-          margin: EdgeInsets.only(left: 20, right: 20, top: 40),
-          decoration: BoxDecoration(
-            boxShadow: [
-              BoxShadow(
-                  color: Colors.black.withOpacity(0.11),
-                  blurRadius: 20,
-                  spreadRadius: 0)
-            ],
-          ),
-          child: TextField(
-            decoration: InputDecoration(
-                prefixIcon: Padding(
-                  padding: const EdgeInsets.all(15.0),
-                  child: SvgPicture.asset('assets/icons/Search.svg'),
-                ),
-                suffixIcon: Container(
-                  width: 100,
-                  child: IntrinsicHeight(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        VerticalDivider(
-                          color: Colors.black,
-                          thickness: 0.3,
-                          indent: 5,
-                          endIndent: 5,
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: SvgPicture.asset('assets/icons/Filter.svg'),
-                        ),
-                      ],
+  SizedBox category_items() {
+    return SizedBox(
+          height: 120,
+          child: ListView.separated(
+            itemCount: categories.length,
+            scrollDirection: Axis.horizontal,
+            padding: EdgeInsets.only(left: 20, right: 20),
+            itemBuilder: (BuildContext context, int index) {
+              return Container(
+                width: 100,
+                decoration: BoxDecoration(
+                    color: categories[index].boxColor.withOpacity(0.3),
+                    borderRadius: BorderRadius.circular(16)),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Container(
+                      width: 50,
+                      height: 50,
+                      decoration: BoxDecoration(
+                          color: Colors.white, shape: BoxShape.circle),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: SvgPicture.asset(categories[index].iconPath),
+                      ),
                     ),
-                  ),
+                    
+                    Text(categories[index].name,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 14,
+                      color: Colors.black
+                    ),)
+                  ],
                 ),
-                filled: true,
-                fillColor: Colors.white,
-                border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(15),
-                    borderSide: BorderSide.none),
-                hintText: 'search for cupcake',
-                hintStyle: TextStyle(
-                  color: const Color.fromARGB(255, 150, 149, 149),
-                  fontSize: 14,
-                )),
+              );
+            },
+            separatorBuilder: (BuildContext context, int index) =>
+                const SizedBox(
+              width: 20,
+            ),
           ),
         );
+  }
+
+  Container _searchField() {
+    return Container(
+      margin: const EdgeInsets.only(left: 20, right: 20, top: 40),
+      decoration: BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+              color: Colors.black.withOpacity(0.11),
+              blurRadius: 20,
+              spreadRadius: 0)
+        ],
+      ),
+      child: TextField(
+        decoration: InputDecoration(
+            prefixIcon: Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: SvgPicture.asset('assets/icons/Search.svg'),
+            ),
+            suffixIcon: SizedBox(
+              width: 100,
+              child: IntrinsicHeight(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    const VerticalDivider(
+                      color: Colors.black,
+                      thickness: 0.3,
+                      indent: 5,
+                      endIndent: 5,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: SvgPicture.asset('assets/icons/Filter.svg'),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            filled: true,
+            fillColor: Colors.white,
+            border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(15),
+                borderSide: BorderSide.none),
+            hintText: 'search for cupcake',
+            hintStyle: const TextStyle(
+              color: Color.fromARGB(255, 150, 149, 149),
+              fontSize: 14,
+            )),
+      ),
+    );
   }
 
   AppBar appBar() {
@@ -97,11 +167,11 @@ class HomePage extends StatelessWidget {
             color: const Color.fromARGB(255, 255, 255, 255),
             borderRadius: BorderRadius.circular(10),
           ),
+          margin: const EdgeInsets.all(10),
+          alignment: Alignment.center,
           child: SvgPicture.asset(
             'assets/icons/Arrow - Left 2.svg',
           ),
-          margin: EdgeInsets.all(10),
-          alignment: Alignment.center,
         ),
       ),
       actions: [
@@ -112,14 +182,14 @@ class HomePage extends StatelessWidget {
               color: const Color.fromARGB(255, 255, 255, 255),
               borderRadius: BorderRadius.circular(10),
             ),
+            margin: const EdgeInsets.all(10),
+            alignment: Alignment.center,
             child: Padding(
               padding: const EdgeInsets.all(8.0),
               child: SvgPicture.asset(
                 'assets/icons/dots.svg',
               ),
             ),
-            margin: EdgeInsets.all(10),
-            alignment: Alignment.center,
           ),
         ),
       ],
