9659ed4ae5e0a1fd630e48c6cde25eb22d7280c6
Faseem7 <76933640+Faseem7@users.noreply.github.com>
2023-09-24 17:37:57 +0530

    added search field

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,9 +1,128 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: appBar(),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          _searchField(),
+          SizedBox(height: 20,),
+          Column(
+            children: [
+              Text('Category',
+              style: TextStyle(
+                color: Colors.black,
+                             ),)
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Container _searchField() {
+    return Container(
+          margin: EdgeInsets.only(left: 20, right: 20, top: 40),
+          decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.black.withOpacity(0.11),
+                  blurRadius: 20,
+                  spreadRadius: 0)
+            ],
+          ),
+          child: TextField(
+            decoration: InputDecoration(
+                prefixIcon: Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: SvgPicture.asset('assets/icons/Search.svg'),
+                ),
+                suffixIcon: Container(
+                  width: 100,
+                  child: IntrinsicHeight(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        VerticalDivider(
+                          color: Colors.black,
+                          thickness: 0.3,
+                          indent: 5,
+                          endIndent: 5,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: SvgPicture.asset('assets/icons/Filter.svg'),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                filled: true,
+                fillColor: Colors.white,
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                    borderSide: BorderSide.none),
+                hintText: 'search for cupcake',
+                hintStyle: TextStyle(
+                  color: const Color.fromARGB(255, 150, 149, 149),
+                  fontSize: 14,
+                )),
+          ),
+        );
+  }
+
+  AppBar appBar() {
+    return AppBar(
+      title: const Text(
+        'Breakfast',
+        style: TextStyle(
+          color: Colors.black,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      elevation: 0,
+      centerTitle: true,
+      backgroundColor: Colors.white,
+      leading: GestureDetector(
+        onTap: () {},
+        child: Container(
+          decoration: BoxDecoration(
+            color: const Color.fromARGB(255, 255, 255, 255),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: SvgPicture.asset(
+            'assets/icons/Arrow - Left 2.svg',
+          ),
+          margin: EdgeInsets.all(10),
+          alignment: Alignment.center,
+        ),
+      ),
+      actions: [
+        GestureDetector(
+          onTap: () {},
+          child: Container(
+            decoration: BoxDecoration(
+              color: const Color.fromARGB(255, 255, 255, 255),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SvgPicture.asset(
+                'assets/icons/dots.svg',
+              ),
+            ),
+            margin: EdgeInsets.all(10),
+            alignment: Alignment.center,
+          ),
+        ),
+      ],
+    );
   }
 }
